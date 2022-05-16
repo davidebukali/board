699494dae9bf1ad35478f18597f3b31707c6d383
David Ebukali <debukali@gmail.com>
2022-05-16 21:07:11 +0300

    Project setup

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="home">React Application</div>
+            <div className="react-logo"></div>
+        </div>
+    );
+}
+
+export default Home
